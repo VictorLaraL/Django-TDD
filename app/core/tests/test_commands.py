a8a96b660fb4b4c90ebbcbdc449d8103fa540d14
Victor Lara <vicblack15@hotmail.com>
2023-01-23 10:10:53 -0600

    fix: Syntax errors.

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -19,9 +19,9 @@ class CommandTest(SimpleTestCase):
         patched_check.return_value = True
 
         call_command('wait_for_db')
-        
+
         patched_check.assert_called_once_with(databases=['default'])
-    
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """ Test waiting for databasse when getting OpertaionalError. """
@@ -32,4 +32,3 @@ class CommandTest(SimpleTestCase):
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-          
