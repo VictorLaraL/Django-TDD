c2dbfc97b7e84e8fb16b690bbc643ed7c046e373
Victor Lara <vicblack15@hotmail.com>
2023-01-23 15:39:16 -0600

    feat: add mew user model configurations and test for it

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,3 +21,35 @@ class ModelTest(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """ Test email is normalized for new users. """
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['test2@Example.com', 'test2@example.com'],
+            ['test3@EXAMPLE.COM', 'test3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            # Only serialized the secon part of the email @example.com
+            # tha name of the mail not change
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """ Test that create a user without an email raises a ValueError """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """ Test that create an super user. """
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password,
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
