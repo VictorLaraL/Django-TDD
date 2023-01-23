1fb24e21a0d728074c59f5188126f635629a5638
Victor Lara <vicblack15@hotmail.com>
2023-01-23 12:36:06 -0600

    feat: add configuration to new user and test

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "HOST": os.environ.get('DB_HOST'),
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASS"),                       
+        "PASSWORD": os.environ.get("DB_PASS"),
     }
 }
 
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
