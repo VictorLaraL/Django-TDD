89c66148554167243525faa4281c315039dc2747
Victor Manuel Lara Lopez <vicblack15@hotmail.com>
2023-02-09 23:41:14 +0000

    feat: Create a new model Tag

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='test123'):
+    """ Create and return a new user. """
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
     """ Test models. """
 
@@ -69,3 +74,10 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """ Test to create a new Tag. """
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
